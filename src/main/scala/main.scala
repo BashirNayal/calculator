bdab802dd242a5c6e1fe59e197b2d635e92e6ecf
Bashir <bashirnayal.99@gmail.com>
2021-02-13 02:05:56 +0100

    comment

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,22 @@
 package calculator
 
+import processing.core.PApplet
+
 object main {
+
+
+
+
+
   def main(args: Array[String]) {
     println(args.mkString(" "))
     val calc = new CalcIntegral
     calc.run()
+
+    val processingArgs = Array("GUI")
+    val mySketch = new GUI()
+//    PApplet.runSketch(processingArgs, mySketch)
+
   }
 }
 //added a comment
